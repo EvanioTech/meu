1fb9529f2c45dcfcf1a5d2d50f1a66f7aae5241e
Evanio Tech <evanioshark@gmail.com>
2025-01-09 08:22:20 -0300

    apredendo props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+
+const Bemvindo = (props) => <h2>Bem-vindo {props.nome}</h2>
+
 function App(){
     return(
         <div>
+            <Bemvindo nome= 'Matias'/>
             <h1>Olá mundo!</h1>
         </div>
     ) 
