3a4aa1e07b9bda57f8aa400e847cea82cdbd11a9
Evanio Tech <evanioshark@gmail.com>
2025-01-10 10:20:40 -0300

    usando operador ternario

---
+++ src/App6.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Membro from './components/Membro';
+
+class App6 extends Component{
+
+   
+
+
+    render(){
+        return(
+            <div>
+               <Membro nome= 'visitante'/>
+            </div>
+        );
+    }
+}
+
+export default App6;
---
+++ src/App7.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class App extends Component{
+
+
+    constructor(props){
+        super(props);
+        this.state ={
+            status:1
+
+        };
+    }
+
+
+    render(){
+        return(
+            <div>
+                {
+                    this.state.status === 1 &&
+                    <h1> Bem-vindo ao sistema!</h1>
+                }
+
+<div>
+                <h2>Curso React js</h2>
+            </div>
+        
+    
+            </div>
+            );
+           
+    }
+}
+
+export default App;
+
